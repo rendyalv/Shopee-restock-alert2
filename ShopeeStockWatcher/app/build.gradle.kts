60a7a1b474cb517214942eb5334d9d2d8febccdd
rendyalv <50459207+rendyalv@users.noreply.github.com>
2025-09-10 17:50:56 +0700

    Update build.gradle.kts

--- ShopeeStockWatcher/app/build.gradle.kts
+++ ShopeeStockWatcher/app/build.gradle.kts
@@ -41,6 +41,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation "androidx.work:work-runtime-ktx:2.9.0"
+    implementation "androidx.core:core-ktx:1.12.0"
+    implementation "com.jakewharton.timber:timber:5.0.1"
+    implementation "com.squareup.retrofit2:retrofit:2.9.0"
+    implementation "com.squareup.retrofit2:converter-gson:2.9.0"
 
     // ✅ OkHttp for networking
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
