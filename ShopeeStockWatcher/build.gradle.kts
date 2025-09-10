f7762b3ff97d41859b42013cbbd0519a807a968c
rendyalv <50459207+rendyalv@users.noreply.github.com>
2025-09-10 15:55:30 +0700

    Update build.gradle.kts

--- ShopeeStockWatcher/build.gradle.kts
+++ ShopeeStockWatcher/build.gradle.kts
@@ -2,3 +2,21 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     kotlin("android") version "1.9.10" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
